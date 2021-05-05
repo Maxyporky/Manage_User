9b291751d1624c933973e8f6d13084702c8c6765
Maxyporky <83421820+Maxyporky@users.noreply.github.com>
2021-05-05 23:25:07 +0200

    Delete files directory

--- files/user/functions/sc_subopt1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-function subopt1 {
-	echo -e "  ${BOLD}${YELLOW}Usuaris disponibles${NC}"
-	echo "----------------------------------------------"
-	awk -F: '{if($3>=1000)print "  "$1}' /etc/passwd
-	echo "----------------------------------------------"
-	echo " "
-	
- }
